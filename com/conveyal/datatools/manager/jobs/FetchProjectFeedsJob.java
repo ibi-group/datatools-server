950565f206a926ed7cba4b363f830f1806cc528e
Landon Reed <landontreed@gmail.com>
2016-03-28 15:53:44 -0400

    fetch project feeds job

--- com/conveyal/datatools/manager/jobs/FetchProjectFeedsJob.java
+++ com/conveyal/datatools/manager/jobs/FetchProjectFeedsJob.java
@@ -1,7 +1,40 @@
 package com.conveyal.datatools.manager.jobs;
 
+import com.conveyal.datatools.manager.models.FeedSource;
+import com.conveyal.datatools.manager.models.FeedVersion;
+import com.conveyal.datatools.manager.models.Project;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by landon on 3/25/16.
  */
-public class FetchProjectFeedsJob {
+public class FetchProjectFeedsJob implements Runnable {
+    public static final Logger LOG = LoggerFactory.getLogger(FetchProjectFeedsJob.class);
+    private Project proj;
+    public Map<String, FeedVersion> result;
+
+    public FetchProjectFeedsJob (Project proj) {
+        this.proj = proj;
+    }
+
+    @Override
+    public void run() {
+        LOG.info("fetch job running for proj: " + proj.name);
+        result = new HashMap<>();
+
+        for(FeedSource feedSource : proj.getFeedSources()) {
+            LOG.info(" " + feedSource.retrievalMethod);
+            if (!FeedSource.FeedRetrievalMethod.FETCHED_AUTOMATICALLY.equals(feedSource.retrievalMethod))
+                continue;
+
+            FeedVersion feedVersion = feedSource.fetch();
+            LOG.info("Fetching feed for " + feedSource.id);
+            result.put(feedSource.id, feedVersion);
+        }
+    }
+
 }
