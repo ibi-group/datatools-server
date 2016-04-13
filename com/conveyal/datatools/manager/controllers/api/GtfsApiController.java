e331c8b38d90c63f944dd3fe4e088927179de509
Landon Reed <landontreed@gmail.com>
2016-04-13 11:10:02 -0400

    swap datatools-common for local navbar

---
+++ com/conveyal/datatools/manager/controllers/api/GtfsApiController.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.manager.controllers.api;
+
+/**
+ * Created by landon on 4/12/16.
+ */
+public class GtfsApiController {
+}
---
+++ com/conveyal/datatools/manager/utils/FeedUpdater.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.manager.utils;
+
+/**
+ * Created by landon on 4/12/16.
+ */
+public class FeedUpdater {
+}
