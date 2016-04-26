aa5105d338665468d41f43a4e4fe2c373a21c639
Landon Reed <landontreed@gmail.com>
2016-04-26 15:53:52 -0400

    added cancel and maxLength functionality

---
+++ com/conveyal/datatools/manager/auth/Auth0Users.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.manager.auth;
+
+/**
+ * Created by landon on 4/26/16.
+ */
+public class Auth0Users {
+}
---
+++ com/conveyal/datatools/manager/utils/NotificationsUtils.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.manager.utils;
+
+/**
+ * Created by landon on 4/26/16.
+ */
+public class NotificationsUtils {
+}
