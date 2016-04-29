11fcef8ff5719b4ec8fac9aa80778b1834684423
Landon Reed <landontreed@gmail.com>
2016-04-29 18:27:27 -0400

    updated load feeds

--- com/conveyal/datatools/manager/controllers/api/GtfsApiController.java
+++ com/conveyal/datatools/manager/controllers/api/GtfsApiController.java
@@ -1,6 +1,7 @@
 package com.conveyal.datatools.manager.controllers.api;
 
 import com.conveyal.datatools.manager.DataManager;
+import com.conveyal.datatools.manager.models.FeedSource;
 import com.conveyal.datatools.manager.utils.FeedUpdater;
 import com.conveyal.gtfs.api.ApiMain;
 import com.conveyal.gtfs.api.Routes;
@@ -17,6 +18,7 @@ import java.util.List;
 public class GtfsApiController {
     public static final Logger LOG = LoggerFactory.getLogger(ProjectController.class);
     public static String feedBucket;
+    public static FeedUpdater feedUpdater;
     public static String prefix;
     public static void register (String apiPrefix) throws IOException {
 
@@ -26,7 +28,7 @@ public class GtfsApiController {
         // check for use extension...
         String extensionType = DataManager.config.get("modules").get("gtfsapi").get("use_extension").asText();
         // if use extension, don't worry about setting up controller to store service alerts.
-        if (extensionType != null){
+        if (extensionType != "false" && extensionType != null){
             LOG.info("Using extension " + extensionType + " for service alerts module");
             feedBucket = DataManager.config.get("extensions").get(extensionType).get("s3_bucket").asText();
             prefix = DataManager.config.get("extensions").get(extensionType).get("s3_download_prefix").asText();
@@ -35,14 +37,21 @@ public class GtfsApiController {
             eTagList.addAll(ApiMain.initialize(null, false, feedBucket, null, null, prefix));
 
             // set feedUpdater to poll for new feeds every half hour
-            FeedUpdater feedUpdater = new FeedUpdater(eTagList, 0, DataManager.config.get("modules").get("gtfsapi").get("update_frequency").asInt());
+            feedUpdater = new FeedUpdater(eTagList, 0, DataManager.config.get("modules").get("gtfsapi").get("update_frequency").asInt());
         }
         // else, set up GTFS Api to use normal data storage
         else {
             LOG.warn("No extension provided");
             // if work_offline, use local directory
+            List<String> feeds = new ArrayList<>();
+            for (FeedSource fs : FeedSource.getAll()) {
+//                feeds.add(fs.getLatestVersionId());
+//                feeds.add(fs.id + ".zip");
+            }
+            String[] feedList = feeds.toArray(new String[0]);
             if (!DataManager.config.get("application").get("data").get("use_s3_storage").asBoolean()) {
-                ApiMain.initialize(DataManager.config.get("application").get("data").get("gtfs").asText());
+                String dir = DataManager.config.get("application").get("data").get("gtfs").asText();
+                eTagList.addAll(ApiMain.initialize(dir, feedList));
             }
 //         else, use s3
             else {
@@ -52,7 +61,7 @@ public class GtfsApiController {
                 eTagList.addAll(ApiMain.initialize(null, false, feedBucket, null, null, null));
 
                 // set feedUpdater to poll for new feeds every half hour
-                FeedUpdater feedUpdater = new FeedUpdater(eTagList, 0, DataManager.config.get("modules").get("gtfsapi").get("update_frequency").asInt());
+                feedUpdater = new FeedUpdater(eTagList, 0, DataManager.config.get("modules").get("gtfsapi").get("update_frequency").asInt());
             }
         }
 
