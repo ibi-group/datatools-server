8224122c0d1a482742e73bd195b9c26186e51344
Landon Reed <landontreed@gmail.com>
2016-05-19 16:29:05 -0400

    logout/in fixes for login from any url path + logout redirection

---
+++ com/conveyal/datatools/manager/controllers/api/DeploymentController.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.manager.controllers.api;
+
+/**
+ * Created by landon on 5/18/16.
+ */
+public class DeploymentController {
+}
---
+++ com/conveyal/datatools/manager/utils/DeploymentManager.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.manager.utils;
+
+/**
+ * Created by landon on 5/18/16.
+ */
+public class DeploymentManager {
+}
