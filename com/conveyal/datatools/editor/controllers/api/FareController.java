547dec31e6eb44fcd5a4928b6cc132e22257a6d5
Landon Reed <landontreed@gmail.com>
2016-06-23 09:52:35 -0400

    major front end editor updates (timetable view, trip patterns, entity creation

---
+++ com/conveyal/datatools/editor/controllers/api/FareController.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.editor.controllers.api;
+
+/**
+ * Created by landon on 6/22/16.
+ */
+public class FareController {
+}
---
+++ com/conveyal/datatools/editor/models/transit/Fare.java
@@ -0,0 +1,7 @@
+package com.conveyal.datatools.editor.models.transit;
+
+/**
+ * Created by landon on 6/22/16.
+ */
+public class Fare {
+}
