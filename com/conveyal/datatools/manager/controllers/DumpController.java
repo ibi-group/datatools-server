39c1eb796c00a144045d3837448490fbaf356907
Landon Reed <landontreed@gmail.com>
2016-09-13 12:30:58 -0400

    add dump method to controller

--- com/conveyal/datatools/manager/controllers/DumpController.java
+++ com/conveyal/datatools/manager/controllers/DumpController.java
@@ -1,15 +1,26 @@
 package com.conveyal.datatools.manager.controllers;
 
+import com.conveyal.datatools.manager.auth.Auth0UserProfile;
+import com.conveyal.datatools.manager.auth.Auth0Users;
+import com.conveyal.datatools.manager.models.Deployment;
 import com.conveyal.datatools.manager.models.FeedSource;
 import com.conveyal.datatools.manager.models.FeedVersion;
+import com.conveyal.datatools.manager.models.JsonViews;
+import com.conveyal.datatools.manager.models.Note;
 import com.conveyal.datatools.manager.models.Project;
+import com.conveyal.datatools.manager.utils.json.JsonManager;
 import com.conveyal.datatools.manager.utils.json.JsonUtil;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import spark.Request;
 import spark.Response;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
@@ -21,6 +32,76 @@ import static spark.Spark.*;
  */
 
 public class DumpController {
+    /**
+     * Represents a snapshot of the database. This require loading the entire database into RAM.
+     * This shouldn't be an issue, though, as the feeds are stored separately. This is only metadata.
+     */
+    public static class DatabaseState {
+        public Collection<Project> projects;
+        public Collection<FeedSource> feedSources;
+        public Collection<FeedVersion> feedVersions;
+        public Collection<Note> notes;
+        //        public Collection<Auth0UserProfile> users;
+        public Collection<Deployment> deployments;
+    }
+
+    private static JsonManager<DatabaseState> json =
+            new JsonManager<DatabaseState>(DatabaseState.class, JsonViews.DataDump.class);
+
+    public static DatabaseState dump (Request req, Response res) throws JsonProcessingException {
+        DatabaseState db = new DatabaseState();
+        db.projects = Project.getAll();
+        db.feedSources = FeedSource.getAll();
+        db.feedVersions = FeedVersion.getAll();
+        db.notes = Note.getAll();
+//        db.users = Auth0Users.getAll();
+        db.deployments = Deployment.getAll();
+
+        return db;
+    }
+
+    // this is not authenticated, because it has to happen with a bare database (i.e. no users)
+    // this method in particular is coded to allow up to 500MB of data to be posted
+//    @BodyParser.Of(value=BodyParser.Json.class, maxLength = 500 * 1024 * 1024)
+//    public static Result load () throws JsonParseException, JsonMappingException, IOException {
+//        // TODO: really ought to check all tables
+//        if (User.usersExist())
+//            return badRequest("database not empty");
+//
+//        DatabaseState db = json.read(request().body().asJson());
+//
+//        for (FeedCollection c : db.feedCollections) {
+//            c.save(false);
+//        }
+//        FeedCollection.commit();
+//
+//        for (FeedSource s : db.feedSources) {
+//            s.save(false);
+//        }
+//        FeedSource.commit();
+//
+//        for (FeedVersion v : db.feedVersions) {
+//            v.save(false);
+//        }
+//        FeedVersion.commit();
+//
+//        for (Note n : db.notes) {
+//            n.save(false);
+//        }
+//        Note.commit();
+//
+//        for (User u : db.users) {
+//            u.save(false);
+//        }
+//        User.commit();
+//
+//        for (Deployment d : db.deployments) {
+//            d.save(false);
+//        }
+//        Deployment.commit();
+//
+//        return ok("done");
+//    }
 
     public static boolean loadLegacy (Request req, Response res) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
@@ -115,8 +196,9 @@ public class DumpController {
     }
 
     public static void register (String apiPrefix) {
-        post(apiPrefix + "loadLegacy", DumpController::loadLegacy, JsonUtil.objectMapper::writeValueAsString);
-        post(apiPrefix + "validateAll", DumpController::validateAll, JsonUtil.objectMapper::writeValueAsString);
+        post(apiPrefix + "loadLegacy", DumpController::loadLegacy, json::write);
+        post(apiPrefix + "validateAll", DumpController::validateAll, json::write);
+        get(apiPrefix + "dump", DumpController::dump, json::write);
         System.out.println("registered dump w/ prefix " + apiPrefix);
     }
 }
