f7b55aef4beb4903f243717cd5043fbc9e2f58d0
Landon Reed <landontreed@gmail.com>
2016-12-15 16:10:17 -0500

    file download method

--- com/conveyal/datatools/common/utils/SparkUtils.java
+++ com/conveyal/datatools/common/utils/SparkUtils.java
@@ -1,7 +1,41 @@
 package com.conveyal.datatools.common.utils;
 
+import spark.Response;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import static spark.Spark.halt;
+
 /**
  * Created by landon on 12/15/16.
  */
 public class SparkUtils {
+
+    public static Object downloadFile(File file, Response res) {
+        if(file == null) halt(404, "File is null");
+
+        res.raw().setContentType("application/octet-stream");
+        res.raw().setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+
+        try {
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(res.raw().getOutputStream());
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = bufferedInputStream.read(buffer)) > 0) {
+                bufferedOutputStream.write(buffer, 0, len);
+            }
+
+            bufferedOutputStream.flush();
+            bufferedOutputStream.close();
+        } catch (Exception e) {
+            halt(500, "Error serving GTFS file");
+        }
+
+        return res.raw();
+    }
 }
