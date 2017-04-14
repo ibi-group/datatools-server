05884e40f2d01dcd63286c78f6deca87ff621097
Landon Reed <landontreed@gmail.com>
2017-04-14 10:25:45 -0400

    test(DatatoolsTest): use env.yml.tmp

--- src/test/java/com/conveyal/datatools/DatatoolsTest.java
+++ src/test/java/com/conveyal/datatools/DatatoolsTest.java
@@ -20,7 +20,7 @@ public abstract class DatatoolsTest {
             return;
         }
         LOG.info("DatatoolsTest setup");
-        String[] args = {};
+        String[] args = {"configurations/default/env.yml.tmp", "configurations/default/server.yml"};
         try {
             DataManager.main(args);
             setUpIsDone = true;
