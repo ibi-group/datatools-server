8fffb185170ba9e6357cca0ea71b2a18db7778f5
Landon Reed <landontreed@gmail.com>
2017-10-19 12:50:18 -0400

    add parentJobId and logic to govern execution of subtasks in case of error

--- src/main/java/com/conveyal/datatools/common/status/MonitorableJob.java
+++ src/main/java/com/conveyal/datatools/common/status/MonitorableJob.java
@@ -1,9 +1,6 @@
 package com.conveyal.datatools.common.status;
 
 import com.conveyal.datatools.manager.DataManager;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.eventbus.EventBus;
-import com.google.common.eventbus.Subscribe;
 import org.eclipse.jetty.util.ConcurrentHashSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +20,7 @@ public abstract class MonitorableJob implements Runnable {
     // Public fields will be serialized over HTTP API and visible to the web client
     public final String name;
     public final JobType type;
+    public String parentJobId;
     // Not final to allow some jobs to have extra status fields.
     public Status status = new Status();
     public final String jobId = UUID.randomUUID().toString();
@@ -31,7 +29,7 @@ public abstract class MonitorableJob implements Runnable {
      * Additional jobs that will be run after the main logic of this job has completed.
      * This job is not considered entirely completed until its sub-jobs have all completed.
      */
-    protected List<Runnable> subJobs = new ArrayList<>();
+    protected List<MonitorableJob> subJobs = new ArrayList<>();
 
     public enum JobType {
         UNKNOWN_TYPE,
@@ -71,7 +69,7 @@ public abstract class MonitorableJob implements Runnable {
     }
 
     /**
-     * This method should never be called directly or overriden. It is a standard clean up stage for all
+     * This method should never be called directly or overridden. It is a standard clean up stage for all
      * monitorable jobs.
      */
     private void unRegisterJob () {
@@ -98,36 +96,86 @@ public abstract class MonitorableJob implements Runnable {
      * override jobLogic and jobFinished method(s).
      */
     public void run () {
+        boolean parentJobErrored = false;
+        boolean subTaskErrored = false;
+        String cancelMessage = "";
         try {
             // First execute the core logic of the specific MonitorableJob subclass
             jobLogic();
-
+            if (status.error) {
+                parentJobErrored = true;
+                cancelMessage = String.format("Task cancelled due to error in %s task", getClass().getSimpleName());
+            }
             // Immediately run any sub-jobs in sequence in the current thread.
             // This hogs the current thread pool thread but makes execution order predictable.
             int subJobNumber = 1;
             int subJobsTotal = subJobs.size() + 1;
-            for (Runnable subJob : subJobs) {
-                status.message = String.format("Sub-job %d/%d", subJobNumber, subJobsTotal);
-                status.percentComplete = subJobNumber * 100D / subJobsTotal;
-                status.error = false;
-                subJob.run();
+
+            for (MonitorableJob subJob : subJobs) {
+                if (!parentJobErrored && !subTaskErrored) {
+                    // Run sub-task if no error has errored during parent job or previous sub-task execution.
+                    // FIXME this will overwrite a message if message is set somewhere else.
+                    // FIXME If a subtask fails, cancel the parent task and cancel or remove subsequent sub-tasks.
+//                status.message = String.format("Finished %d/%d sub-tasks", subJobNumber, subJobsTotal);
+                    status.percentComplete = subJobNumber * 100D / subJobsTotal;
+                    status.error = false; // FIXME: remove this error=false assignment
+                    subJob.run();
+
+                    // Record if there has been an error in the execution of the sub-task. (Note: this will not
+                    // incorrectly overwrite a 'true' value with 'false' because the sub-task is only run if
+                    // jobHasErrored is false.
+                    if (subJob.status.error) {
+                        subTaskErrored = true;
+                        cancelMessage = String.format("Task cancelled due to error in %s task", subJob.getClass().getSimpleName());
+                    }
+                } else {
+                    // Cancel (fail) next sub-task and continue.
+                    subJob.cancel(cancelMessage);
+                }
                 subJobNumber += 1;
             }
+            // FIXME: should jobFinished be run if task or any sub-task fails?
+            if (subTaskErrored) {
+                // Cancel parent job completion if an error was encountered in task/sub-task. No need to cancel sub-task
+                // because the error presumably already occurred and has a better error message.
+                cancel(cancelMessage);
+            }
+
+            // Run final steps of job pending completion or error. Note: any tasks that depend on job success should
+            // check job status to determine if final step should be executed (e.g., storing feed version in MongoDB).
+            // TODO: should we add separate hooks depending on state of job/sub-tasks (e.g., success, catch, finally)
             jobFinished();
-            // TODO Mark the status in the event bus as "finished". Currently we always immediately delete the job so this is never seen.
-            unRegisterJob();
+
+            status.completed = true;
+
+            // We retain finished or errored jobs on the server until they are fetched via the API, which implies they
+            // could be displayed by the client.
         } catch (Exception ex) {
             // Set job status to failed
-            // Note that when an exception occurs we do not call unRegisterJob,
+            // Note that when an exception occurs during job execution we do not call unRegisterJob,
             // so the job continues to exist in the failed state and the user can see it.
-            // TODO somehow delete it later
+            status.update(true, ex.getMessage(), 100, true);
         }
+    }
+
+    /**
+     * An alternative method to run(), this method updates job status with error and should contain any other
+     * clean up steps needed to complete job in an errored state (generally due to failure in a previous task in
+     * the chain).
+     */
+    private void cancel(String message) {
+        // Updating the job status with error is all we need to do in order to move the job into completion. Once the
+        // user fetches the errored job, it will be automatically removed from the system.
+        status.update(true, message, 100);
+        status.completed = true;
+        // FIXME: Do we need to run any clean up here?
     }
 
     /**
      * Enqueues a sub-job to be run when the main logic of this job has finished.
      */
-    public void addNextJob(Runnable job) {
+    public void addNextJob(MonitorableJob job) {
+        job.parentJobId = this.jobId;
         subJobs.add(job);
     }
 
@@ -138,6 +186,10 @@ public abstract class MonitorableJob implements Runnable {
 
         /** What message (defined in messages.<lang>) should be displayed to the user? */
         public String message;
+
+        /** Detailed exception method to display to user (to help with support requests) */
+        public String exceptionType;
+        public String exceptionDetails;
 
         /** Is this deployment completed (successfully or unsuccessfully) */
         public boolean completed = false;
@@ -167,6 +219,13 @@ public abstract class MonitorableJob implements Runnable {
             this.error = isError;
             this.message = message;
             this.percentComplete = percentComplete;
+        }
+
+        public void update (boolean isError, String message, double percentComplete, boolean isComplete) {
+            this.error = isError;
+            this.message = message;
+            this.percentComplete = percentComplete;
+            this.completed = isComplete;
         }
 
     }
