965c45f8284a642607e138e703ef968675f03ba4
Landon Reed <landontreed@gmail.com>
2018-05-23 09:06:20 -0400

    handle unknown properties in otp configs

--- src/main/java/com/conveyal/datatools/manager/models/OtpBuildConfig.java
+++ src/main/java/com/conveyal/datatools/manager/models/OtpBuildConfig.java
@@ -1,11 +1,13 @@
 package com.conveyal.datatools.manager.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 
 /**
  * Created by demory on 3/8/15.
  */
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OtpBuildConfig implements Serializable {
     private static final long serialVersionUID = 1L;
 
--- src/main/java/com/conveyal/datatools/manager/models/OtpRouterConfig.java
+++ src/main/java/com/conveyal/datatools/manager/models/OtpRouterConfig.java
@@ -1,12 +1,14 @@
 package com.conveyal.datatools.manager.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 import java.util.Collection;
 
 /**
  * Created by demory on 3/8/15.
  */
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OtpRouterConfig implements Serializable {
     private static final long serialVersionUID = 1L;
     public Integer numItineraries;
