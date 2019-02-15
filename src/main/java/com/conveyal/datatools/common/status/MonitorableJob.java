2fc62d1a0507828967184d0139ef42147ac1d0e2
Landon Reed <landontreed@gmail.com>
2019-02-15 15:04:26 -0500

    refactor(job): remove unused job type

--- src/main/java/com/conveyal/datatools/common/status/MonitorableJob.java
+++ src/main/java/com/conveyal/datatools/common/status/MonitorableJob.java
@@ -57,7 +57,6 @@ public abstract class MonitorableJob implements Runnable {
         EXPORT_SNAPSHOT_TO_GTFS,
         CONVERT_EDITOR_MAPDB_TO_SQL,
         VALIDATE_ALL_FEEDS,
-        MERGE_PROJECT_FEEDS,
         MERGE_FEED_VERSIONS
     }
 
