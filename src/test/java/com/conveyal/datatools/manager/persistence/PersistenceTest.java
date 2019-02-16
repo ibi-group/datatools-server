e174aa39fabe4bc1bf4c1a644a7b7c1551e9309d
evansiroky <evan.siroky@yahoo.com>
2019-02-15 22:20:21 -0800

    refactor(test): change argument order for junit v4 compatibility

--- src/test/java/com/conveyal/datatools/manager/persistence/PersistenceTest.java
+++ src/test/java/com/conveyal/datatools/manager/persistence/PersistenceTest.java
@@ -30,7 +30,7 @@ public class PersistenceTest {
         String id = feedSource.id;
         Persistence.feedSources.create(feedSource);
         String retrievedId = Persistence.feedSources.getById(id).id;
-        assertEquals(retrievedId, id, "Found FeedSource ID should equal inserted ID.");
+        assertEquals("Found FeedSource ID should equal inserted ID.", retrievedId, id);
     }
 
 //    @Test
@@ -59,7 +59,7 @@ public class PersistenceTest {
         String id = project.id;
         Persistence.projects.create(project);
         String retrievedId = Persistence.projects.getById(id).id;
-        assertEquals(retrievedId, id, "Found Project ID should equal inserted ID.");
+        assertEquals("Found Project ID should equal inserted ID.", retrievedId, id);
     }
 //
 //    @Test
