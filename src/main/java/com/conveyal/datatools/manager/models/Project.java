9969a5659aa42caae91442d0d96affc27f40d3cc
evansiroky <evan.siroky@yahoo.com>
2019-03-28 16:40:41 -0700

    refactor(project): improve comment about pinned deployments

--- src/main/java/com/conveyal/datatools/manager/models/Project.java
+++ src/main/java/com/conveyal/datatools/manager/models/Project.java
@@ -63,7 +63,10 @@ public class Project extends Model {
     // and/or for applying a geographic filter when syncing with external feed registries.
     public Bounds bounds;
 
-    // used to identify a specific deployment. A bunch of UI things depend on this.
+    // Identifies a specific "pinned" deployment for the project. This is used in datatools-ui in 2 places:
+    // 1. In the list of project deployments, a "pinned" deployment is shown first and highlighted.
+    // 2. In the project feed source table, if a "pinned" deployment exists, the status of the versions that were in
+    //   the "pinned" deployment are shown and compared to the most recent version in the feed sources.
     public String pinnedDeploymentId;
 
     public Project() {
