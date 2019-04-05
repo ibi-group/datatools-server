1ae7372bacb01abe272da9536266845f739f1f92
evansiroky <evan.siroky@yahoo.com>
2019-04-05 10:36:05 -0700

    ci: inline e2e run check script in travis.yml

--- scripts/check-if-e2e-should-run-on-travis.sh
+++
@@ -1,9 +0,0 @@
-if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
-  if [[ "$TRAVIS_PULL_REQUEST_SLUG" = "conveyal/datatools-server" ]]; then
-    export SHOULD_RUN_E2E=true;
-  fi
-else
-  if [[ "$TRAVIS_REPO_SLUG" = "conveyal/datatools-server" ]] && [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "dev" ]]; then
-    export SHOULD_RUN_E2E=true;
-  fi
-fi
