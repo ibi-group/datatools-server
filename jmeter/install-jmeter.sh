fe36935692366591541000ec2ab975ec928ce90a
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-01-29 16:38:16 -0500

    chore(jmeter): Update download URLs; bump jmeter to v 5.2.1.

--- jmeter/install-jmeter.sh
+++ jmeter/install-jmeter.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # install jmeter
-wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-3.3.zip
-unzip apache-jmeter-3.3.zip
-rm -rf apache-jmeter-3.3.zip
+wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.2.1.zip
+unzip apache-jmeter-5.2.1.zip
+rm -rf apache-jmeter-5.2.1.zip
 
 # install jmeter plugin manager
-wget -O apache-jmeter-3.3/lib/ext/jmeter-plugins-manager-0.16.jar https://jmeter-plugins.org/get/
+wget -O apache-jmeter-5.2.1/lib/ext/jmeter-plugins-manager-0.16.jar https://jmeter-plugins.org/get/
 
 # install command line runner
-wget -O apache-jmeter-3.3/lib/cmdrunner-2.0.jar http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar
+wget -O apache-jmeter-5.2.1/lib/cmdrunner-2.0.jar https://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar
 
 # run jmeter to generate command line script
-java -cp apache-jmeter-3.3/lib/ext/jmeter-plugins-manager-0.16.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
+java -cp apache-jmeter-5.2.1/lib/ext/jmeter-plugins-manager-0.16.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
 
 # install jpgc-json-2
-apache-jmeter-3.3/bin/PluginsManagerCMD.sh install jpgc-json
+apache-jmeter-5.2.1/bin/PluginsManagerCMD.sh install jpgc-json
 
 # install jar file for commons csv
-wget -O apache-jmeter-3.3/lib/ext/commons-csv-1.5.jar http://central.maven.org/maven2/org/apache/commons/commons-csv/1.5/commons-csv-1.5.jar
+wget -O apache-jmeter-5.2.1/lib/ext/commons-csv-1.5.jar https://repo1.maven.org/maven2/org/apache/commons/commons-csv/1.5/commons-csv-1.5.jar
