5c98a5d9a2b518c0e5483d4e9590ceff140968cd
Landon Reed <landontreed@gmail.com>
2021-01-12 14:29:01 -0500

    ci: echo slugs/repo name

--- scripts/check-if-e2e-tests-should-run-on-travis.sh
+++ scripts/check-if-e2e-tests-should-run-on-travis.sh
@@ -1,6 +1,9 @@
 # Since the e2e tests take a while to run and it could present an inconvenience
 # to be making sure the e2e tests work on every single PR, only run the e2e
 # tests on Travis for PRs to master or on commits directly to dev or master
+echo $GITHUB_BASE_REF_SLUG
+echo $GITHUB_REPOSITORY
+echo $GITHUB_SLUG_REF
 if [[ "$GITHUB_BASE_REF_SLUG" = "master" ]]; then
   echo "SHOULD_RUN_E2E=true" >> $GITHUB_ENV
   echo 'Will run E2E tests because this is a PR to master'
