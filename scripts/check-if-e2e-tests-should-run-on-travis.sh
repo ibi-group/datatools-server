38ce81f614ea932d60b0d26bdad04024d309812f
Landon Reed <landontreed@gmail.com>
2021-01-12 10:49:17 -0500

    ci(github): fix setting e2e env variable

--- scripts/check-if-e2e-tests-should-run-on-travis.sh
+++ scripts/check-if-e2e-tests-should-run-on-travis.sh
@@ -2,11 +2,15 @@
 # to be making sure the e2e tests work on every single PR, only run the e2e
 # tests on Travis for PRs to master or on commits directly to dev or master
 if [[ "$GITHUB_BASE_REF_SLUG" = "master" ]]; then
-  export SHOULD_RUN_E2E=true;
+  echo "SHOULD_RUN_E2E=true" >> $GITHUB_ENV
   echo 'Will run E2E tests because this is a PR to master'
 else
   if [[ "$GITHUB_REPOSITORY" = "ibi-group/datatools-server" ]] && [[ "$GITHUB_SLUG_REF" = "master" || "$GITHUB_SLUG_REF" = "dev" ]]; then
-    export SHOULD_RUN_E2E=true;
+    echo "SHOULD_RUN_E2E=true" >> $GITHUB_ENV
     echo 'Will run E2E tests because this is a commit to master or dev'
   fi
+fi
+
+if [[ "$SHOULD_RUN_E2E" != "true" ]]; then
+  echo 'Skipping E2E tests...'
 fi
