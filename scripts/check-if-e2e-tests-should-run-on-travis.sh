1f05e0dd3612340b888e2d96b9434a718ff4a0fe
Landon Reed <landontreed@gmail.com>
2021-01-12 10:33:40 -0500

    ci(github): fix use of ref slugs

--- scripts/check-if-e2e-tests-should-run-on-travis.sh
+++ scripts/check-if-e2e-tests-should-run-on-travis.sh
@@ -1,11 +1,9 @@
 # Since the e2e tests take a while to run and it could present an inconvenience
 # to be making sure the e2e tests work on every single PR, only run the e2e
 # tests on Travis for PRs to master or on commits directly to dev or master
-if [[ ! -z "$GITHUB_HEAD_REF" ]]; then
-  if [[ "$GITHUB_SLUG_REF" = "master" ]]; then
-    export SHOULD_RUN_E2E=true;
-    echo 'Will run E2E tests because this is a PR to master'
-  fi
+if [[ "$GITHUB_BASE_REF_SLUG" = "master" ]]; then
+  export SHOULD_RUN_E2E=true;
+  echo 'Will run E2E tests because this is a PR to master'
 else
   if [[ "$GITHUB_REPOSITORY" = "ibi-group/datatools-server" ]] && [[ "$GITHUB_SLUG_REF" = "master" || "$GITHUB_SLUG_REF" = "dev" ]]; then
     export SHOULD_RUN_E2E=true;
