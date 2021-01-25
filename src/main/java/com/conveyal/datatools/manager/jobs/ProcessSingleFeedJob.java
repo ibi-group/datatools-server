853862f875d785f399d9dfc8c7b3369f3c05dac8
Rob Beer <rbeer@ibigroup.com>
2021-01-25 15:53:37 +0000

    refactor(ProcessSingleFeedJob): Added the missing DataManager import

--- src/main/java/com/conveyal/datatools/manager/jobs/ProcessSingleFeedJob.java
+++ src/main/java/com/conveyal/datatools/manager/jobs/ProcessSingleFeedJob.java
@@ -2,6 +2,7 @@ package com.conveyal.datatools.manager.jobs;
 
 import com.conveyal.datatools.common.status.MonitorableJob;
 import com.conveyal.datatools.editor.jobs.CreateSnapshotJob;
+import com.conveyal.datatools.manager.DataManager;
 import com.conveyal.datatools.manager.auth.Auth0UserProfile;
 import com.conveyal.datatools.manager.models.FeedSource;
 import com.conveyal.datatools.manager.models.FeedVersion;
