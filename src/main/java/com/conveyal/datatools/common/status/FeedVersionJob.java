fa3d5841ded5fd0ef6f21818125cf040c52a5aff
Robin Beer <rbeer@ibigroup.com>
2021-04-12 09:46:53 +0100

    Update src/main/java/com/conveyal/datatools/common/status/FeedVersionJob.java
    Co-authored-by: Landon Reed <landontreed@gmail.com>

--- src/main/java/com/conveyal/datatools/common/status/FeedVersionJob.java
+++ src/main/java/com/conveyal/datatools/common/status/FeedVersionJob.java
@@ -2,6 +2,9 @@ package com.conveyal.datatools.common.status;
 
 import com.conveyal.datatools.manager.auth.Auth0UserProfile;
 
+/**
+ * This class should be used for any job that operates on a FeedVersion.
+ */
 public abstract class FeedVersionJob extends FeedSourceJob {
     public FeedVersionJob(Auth0UserProfile owner, String name, JobType type) {
         super(owner, name, type);
