06d46fe1bfb894d5faabf0fe6a8f3011f2233257
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2021-05-28 10:49:00 -0400

    test(JsonUtilTest): Reorganize tests.

--- src/test/java/com/conveyal/datatools/manager/utils/JsonUtilTest.java
+++ src/test/java/com/conveyal/datatools/manager/utils/JsonUtilTest.java
@@ -3,38 +3,48 @@ package com.conveyal.datatools.manager.utils;
 import com.conveyal.datatools.manager.models.transform.Substitution;
 import com.conveyal.datatools.manager.utils.json.JsonUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
-import java.util.stream.Stream;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * Test class for JsonUtil.
+ * We use, as an illustration, {@link Substitution} as the target class for JSON parsing.
+ */
 public class JsonUtilTest {
     final static String JSON_LIST = "[{ \"pattern\": \"abc\" }, {\"pattern\": \"def\"}]";
 
     /**
-     * Ensure {@link JsonUtil#getPOJOFromJSONAsList} returns at least an empty list
-     * for the given jsonNode input.
+     * Ensure {@link JsonUtil#getPOJOFromJSONAsList} returns an empty list
+     * if the given jsonNode input is null.
     */
-    @ParameterizedTest
-    @MethodSource("createPOJOFromJSONCases")
-    public void testGetPOJOFromJSONAsListIsNotNull(JsonNode jsonNode) {
-        assertNotNull(JsonUtil.getPOJOFromJSONAsList(
-            jsonNode,
+    @Test
+    public void testGetPOJOFromNullJSONIsNotNull() {
+
+        List<Substitution> list = JsonUtil.getPOJOFromJSONAsList(
+            null,
             Substitution.class
-        ));
+        );
+        assertNotNull(list);
+        assertEquals(0, list.size());
     }
 
-    private static Stream<Arguments> createPOJOFromJSONCases() throws JsonProcessingException {
-        return Stream.of(
-            // Handle cases where JSON config data is missing.
-            Arguments.of((Object) null),
-            // Normal cases where JSON config data is populated.
-            Arguments.of(new ObjectMapper().readTree(JSON_LIST))
+    /**
+     * Ensure {@link JsonUtil#getPOJOFromJSONAsList} populates a list from JSON.
+     */
+    @Test
+    public void testGetPOJOFromJSONAsListIsPopulated() throws JsonProcessingException {
+        List<Substitution> list = JsonUtil.getPOJOFromJSONAsList(
+            new ObjectMapper().readTree(JSON_LIST),
+            Substitution.class
         );
+        assertNotNull(list);
+        assertEquals(2, list.size());
+        assertEquals("abc", list.get(0).pattern);
+        assertEquals("def", list.get(1).pattern);
     }
 }
