664518f453c74b7e7c59fad1e7cb932a2f3b97c4
Philip Cline <philip.cline@ibigroup.com>
2021-12-20 17:00:06 -0500

    Add IBI Group to readme

--- src/main/java/com/conveyal/datatools/manager/DataManager.java
+++ src/main/java/com/conveyal/datatools/manager/DataManager.java
@@ -215,6 +215,7 @@ public class DataManager {
             new EditorControllerImpl(EDITOR_API_PREFIX, Table.LOCATION_GROUPS, DataManager.GTFS_DATA_SOURCE);
             new EditorControllerImpl(EDITOR_API_PREFIX, Table.LOCATION_META_DATA, DataManager.GTFS_DATA_SOURCE);
             new EditorControllerImpl(EDITOR_API_PREFIX, Table.LOCATION_SHAPES, DataManager.GTFS_DATA_SOURCE);
+            new EditorControllerImpl(EDITOR_API_PREFIX, Table.LOCATIONS, DataManager.GTFS_DATA_SOURCE);
             new EditorControllerImpl(EDITOR_API_PREFIX, Table.ROUTES, DataManager.GTFS_DATA_SOURCE);
             // NOTE: Patterns controller handles updates to nested tables shapes, pattern stops, and frequencies.
             new EditorControllerImpl(EDITOR_API_PREFIX, Table.PATTERNS, DataManager.GTFS_DATA_SOURCE);
