2d0233026feab812b622e99b125fde309119ddbc
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-04-08 15:12:18 -0400

    refactor(GtfsUtils): Break out code.

--- src/main/java/com/conveyal/datatools/manager/utils/GtfsUtils.java
+++ src/main/java/com/conveyal/datatools/manager/utils/GtfsUtils.java
@@ -2,7 +2,6 @@ package com.conveyal.datatools.manager.utils;
 
 import com.conveyal.datatools.manager.DataManager;
 import com.conveyal.datatools.manager.gtfsplus.tables.GtfsPlusTable;
-import com.conveyal.datatools.manager.models.FeedVersion;
 import com.conveyal.datatools.manager.models.Snapshot;
 import com.conveyal.datatools.manager.persistence.Persistence;
 import com.conveyal.gtfs.loader.Field;
@@ -17,6 +16,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.mongodb.client.model.Filters.and;
@@ -51,130 +51,86 @@ public class GtfsUtils {
     }
 
     public static void checkReferencedNamespaces() {
-        Persistence.projects.getAll().forEach(
-            p -> {
-                System.out.println("Project " + p.name);
-                Persistence.feedSources.getFiltered(eq("projectId", p.id)).forEach(
-                    fs -> {
-                        System.out.println("- FeedSource " + fs.name + " " + fs.id);
-                        if (!Strings.isNullOrEmpty(fs.editorNamespace)) {
-                            System.out.print("    - editor: " + fs.editorNamespace);
-                            checkTablesForNamespace(fs.editorNamespace);
-                        }
-
-                        Bson feedSourceIdFilter = eq("feedSourceId", fs.id);
-                        Bson feedSourceIdNamespaceFilter = and(
-                            eq("feedSourceId", fs.id),
-                            not(eq("namespace", null))
-                        );
-/*
-                        List<FeedVersion> allFeedVersions = Persistence.feedVersions.getFiltered(feedSourceIdFilter);
-                        List<FeedVersion> feedVersions = Persistence.feedVersions.getFiltered(feedSourceIdNamespaceFilter);
-                        System.out.println("  - FeedVersions (" + feedVersions.size() + "/" + allFeedVersions.size() + " with valid namespace)");
-                        feedVersions.forEach(
-                            fv -> {
-                                System.out.print("    - v" + fv.version + ": " + fv.namespace);
-                                checkTablesForNamespace(fv.namespace);
+        try (Connection connection = DataManager.GTFS_DATA_SOURCE.getConnection()) {
+            Persistence.projects.getAll().forEach(
+                p -> {
+                    System.out.println("Project " + p.name);
+                    Persistence.feedSources.getFiltered(eq("projectId", p.id)).forEach(
+                        fs -> {
+                            System.out.println("- FeedSource " + fs.name + " " + fs.id);
+                            if (!Strings.isNullOrEmpty(fs.editorNamespace)) {
+                                System.out.println("  - editor: " + fs.editorNamespace);
+                                checkTablesForNamespace(fs.editorNamespace, connection);
                             }
-                        );
-*/
-                        List<Snapshot> allSnapshots = Persistence.snapshots.getFiltered(feedSourceIdFilter);
-                        List<Snapshot> snapshots = Persistence.snapshots.getFiltered(feedSourceIdNamespaceFilter);
-                        System.out.println("  - Snapshots (" + snapshots.size() + "/" + allSnapshots.size() + " with valid namespace)");
-                        snapshots.forEach(
-                            sn -> {
-                                System.out.println("    - " + sn.name + " " + sn.id);
-                                System.out.print("      - namespace: " + sn.namespace);
-                                checkTablesForNamespace(sn.namespace);
-                                if (!Strings.isNullOrEmpty(sn.snapshotOf) && !sn.snapshotOf.equals("mapdb_editor")) {
-                                    System.out.print("      - snapshotOf: " + sn.snapshotOf);
-                                    checkTablesForNamespace(sn.snapshotOf);
+
+                            Bson feedSourceIdFilter = eq("feedSourceId", fs.id);
+                            Bson feedSourceIdNamespaceFilter = and(
+                                eq("feedSourceId", fs.id),
+                                not(eq("namespace", null))
+                            );
+    /*
+                            List<FeedVersion> allFeedVersions = Persistence.feedVersions.getFiltered(feedSourceIdFilter);
+                            List<FeedVersion> feedVersions = Persistence.feedVersions.getFiltered(feedSourceIdNamespaceFilter);
+                            System.out.println("  - FeedVersions (" + feedVersions.size() + "/" + allFeedVersions.size() + " with valid namespace)");
+                            feedVersions.forEach(
+                                fv -> {
+                                    System.out.print("    - v" + fv.version + ": " + fv.namespace);
+                                    checkTablesForNamespace(fv.namespace);
                                 }
-                            }
-                        );
+                            );
+    */
+                            List<Snapshot> allSnapshots = Persistence.snapshots.getFiltered(feedSourceIdFilter);
+                            List<Snapshot> snapshots = Persistence.snapshots.getFiltered(feedSourceIdNamespaceFilter);
+                            System.out.println("  - Snapshots (" + snapshots.size() + "/" + allSnapshots.size() + " with valid namespace)");
+                            snapshots.forEach(
+                                sn -> {
+                                    System.out.println("    - " + sn.name + " " + sn.id);
+                                    System.out.println("      - namespace: " + sn.namespace);
+                                    checkTablesForNamespace(sn.namespace, connection);
+                                    if (!Strings.isNullOrEmpty(sn.snapshotOf) && !sn.snapshotOf.equals("mapdb_editor")) {
+                                        System.out.println("      - snapshotOf: " + sn.snapshotOf);
+                                        checkTablesForNamespace(sn.snapshotOf, connection);
+                                    }
+                                }
+                            );
 
-                    }
-                );
-            }
-        );
+                        }
+                    );
+                }
+            );
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
     }
 
-    public static void checkTablesForNamespace(String namespace) {
-        try (Connection connection = DataManager.GTFS_DATA_SOURCE.getConnection()) {
-            // Check that all tables for the namespace are present.
-            PreparedStatement selectNamespaceTablesStatement = connection.prepareStatement(
-                //"select table_name from information_schema.tables where table_schema = ?"
-                "select table_name from metatables where table_schema = ?"
-            );
-            selectNamespaceTablesStatement.setString(1, namespace);
-            PreparedStatement selectLoadedDateStatement = connection.prepareStatement(
-                "select loaded_date from feeds where namespace = ?"
-            );
-            selectLoadedDateStatement.setString(1, namespace);
+    public static void checkTablesForNamespace(String namespace, Connection connection) {
+        try {
+            NamespaceInfo nsInfo = new NamespaceInfo(namespace, connection);
 
-            ResultSet loadedDateSet = selectLoadedDateStatement.executeQuery();
-            String loadedDate = "";
-            while (loadedDateSet.next()) {
-                loadedDate = loadedDateSet.getString(1);
-                break;
-            }
-            // System.out.println("          Loaded date: " + loadedDate);
-
-            ResultSet resultSet = selectNamespaceTablesStatement.executeQuery();
-            List<String> tableNames = new ArrayList<>();
-            while (resultSet.next()) {
-                String tableName = resultSet.getString(1);
-                tableNames.add(tableName);
-                //System.out.printf("          - %s\n", tableName);
-
-/*
-                String updateTableSql = updateSql.replaceAll(NAMESPACE_WILDCARD, namespace);
-                Statement statement = connection.createStatement();
-                try {
-                    System.out.println(updateTableSql);
-                    int updated = statement.executeUpdate(updateTableSql);
-                    System.out.println(String.format("Updated rows: %d", updated));
-                    successCount++;
-                } catch (SQLException e) {
-                    // The stops table likely did not exist for the schema.
-                    e.printStackTrace();
-                    failedNamespace.add(namespace);
-                }
-
- */
-            }
-
-            if (tableNames.isEmpty()) {
-                if (Strings.isNullOrEmpty(loadedDate)) {
+            if (nsInfo.tableNames.isEmpty()) {
+                if (Strings.isNullOrEmpty(nsInfo.loadedDate)) {
                     System.out.println(" - orphan");
                 } else {
                     // Are there tables?
                     System.out.println("\n          No tables found.");
                 }
             } else {
-                System.out.println();
                 // Are tables missing?
+                nsInfo.missingTables.forEach(t -> System.out.println("          Missing table: " + t));
+
                 for (Table table : Table.tablesInOrder) {
-                    if (tableNames.contains(table.name)) {
+                    if (nsInfo.tableNames.contains(table.name)) {
                         // Are fields missing?
                         checkTableColumns(namespace, connection, table);
-                    } else {
-                        System.out.println("          Missing table: " + table.name);
                     }
                 }
             }
 
-
-
-
-
-
         } catch (SQLException sqle) {
-            //
+            // Do nothing
         }
 
-        //
-        //Table.tablesInOrder
+
 
         // If a table is missing, create it?
 
@@ -200,44 +156,122 @@ public class GtfsUtils {
             ));
         }
 
-        System.out.println("          Found table: " + table.name);
-
-        for (Field field : table.fields) {
-            ColumnInfo columnForField = null;
-            for (ColumnInfo column : columns) {
-                if (field.name.equals(column.columnName)) {
-                    columnForField = column;
-                    break;
-                }
-            }
-
-            if (columnForField != null) {
-                // System.out.println("            Found: " + field.name);
-
-                // Are fields that are present of the correct type?
-                if (!field.getSqlTypeName().equals(columnForField.dataType)) {
-                    System.out.println("Incorrect type for column: " + field.name + " expected: " + field.getSqlTypeName() + " actual: " + columnForField.dataType);
-                }
-                // Only the id column seems to be marked as not nullable, so we won't check that for now.
-            } else {
-                System.out.println("            Missing column: " + field.name);
-            }
+        TableInfo tableInfo = new TableInfo(table, columns);
+        if (tableInfo.hasColumnIssues()) {
+            System.out.println("          Issues in table: " + table.name);
+            tableInfo.missingColumns.forEach(c -> System.out.println("            Missing column: " + c));
+            tableInfo.columnsWithWrongType.forEach(
+                c -> System.out.println("            Incorrect type for column: " + c.columnName + " expected: " + c.expectedType + " actual: " + c.dataType)
+            );
         }
     }
 
     public static class ColumnInfo {
         public String columnName;
         public String dataType;
+        public String expectedType;
 
         public ColumnInfo(String columnName, String dataType) {
             this.columnName = columnName;
             this.dataType = dataType;
+
+            // Normalize data types to the ones used by the GTFS-lib tables.
             if (dataType.equals("character varying")) {
                 this.dataType = "varchar";
             }
             if (dataType.equals("ARRAY")) {
                 this.dataType = "text[]";
             }
+        }
+    }
+
+    public static class NamespaceInfo {
+        public final String namespace;
+        private String loadedDate = "";
+        public final List<String> tableNames = new ArrayList<>();
+        public final List<String> missingTables = new ArrayList<>();
+        public final List<String> validTables = new ArrayList<>();
+
+        /** Used for tests only */
+        public NamespaceInfo(String namespace, List<String> excludedTables) {
+            this.namespace = namespace;
+            for (Table table : Table.tablesInOrder) {
+                this.tableNames.add(table.name);
+            }
+            this.tableNames.removeAll(excludedTables);
+            sortTables();
+        }
+
+        public NamespaceInfo(String namespace, Connection connection) throws SQLException {
+            this.namespace = namespace;
+
+            // Check that all tables for the namespace are present.
+            PreparedStatement selectNamespaceTablesStatement = connection.prepareStatement(
+                //"select table_name from information_schema.tables where table_schema = ?"
+                "select table_name from metatables where table_schema = ?"
+            );
+            selectNamespaceTablesStatement.setString(1, namespace);
+            PreparedStatement selectLoadedDateStatement = connection.prepareStatement(
+                "select loaded_date from feeds where namespace = ?"
+            );
+            selectLoadedDateStatement.setString(1, namespace);
+
+            ResultSet loadedDateSet = selectLoadedDateStatement.executeQuery();
+            while (loadedDateSet.next()) {
+                this.loadedDate = loadedDateSet.getString(1);
+                break;
+            }
+
+            ResultSet resultSet = selectNamespaceTablesStatement.executeQuery();
+            while (resultSet.next()) {
+                tableNames.add(resultSet.getString(1));
+            }
+
+            sortTables();
+        }
+
+        private void sortTables() {
+            for (Table table : Table.tablesInOrder) {
+                String tableName = table.name;
+                if (tableNames.contains(tableName)) {
+                    validTables.add(tableName);
+                } else {
+                    missingTables.add(tableName);
+                }
+            }
+        }
+    }
+
+    public static class TableInfo {
+        public final List<ColumnInfo> columns;
+        public final List<String> missingColumns = new ArrayList<>();
+        public final List<ColumnInfo> columnsWithWrongType = new ArrayList<>();
+
+        public TableInfo(Table table, List<ColumnInfo> columns) {
+            this.columns = columns;
+
+            for (Field field : table.fields) {
+                Optional<ColumnInfo> foundColumnForField = columns
+                    .stream()
+                    .filter(c -> c.columnName.equals(field.name))
+                    .findFirst();
+
+                if (foundColumnForField.isPresent()) {
+                    ColumnInfo columnForField = foundColumnForField.get();
+                    // Are fields that are present of the correct type?
+                    if (!field.getSqlTypeName().equals(columnForField.dataType)) {
+                        columnForField.expectedType = field.getSqlTypeName();
+                        columnsWithWrongType.add(columnForField);
+                    }
+                    // Only the id column seems to be marked as not nullable, so we won't check that for now.
+                } else {
+                    missingColumns.add(field.name);
+                }
+            }
+        }
+
+        public boolean hasColumnIssues() {
+            return !missingColumns.isEmpty() || !columnsWithWrongType.isEmpty();
         }
     }
 }
--- src/test/java/com/conveyal/datatools/manager/TableUpdaterTest.java
+++ src/test/java/com/conveyal/datatools/manager/TableUpdaterTest.java
@@ -1,2 +1,55 @@
-package com.conveyal.datatools.manager;public class TableUpdaterTest {
+package com.conveyal.datatools.manager;
+
+import com.conveyal.datatools.DatatoolsTest;
+import com.conveyal.datatools.manager.utils.GtfsUtils;
+import com.conveyal.gtfs.loader.Table;
+import com.google.common.collect.Lists;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TableUpdaterTest extends DatatoolsTest {
+
+    @BeforeAll
+    public static void setUp() throws IOException {
+        DatatoolsTest.setUp();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+
+    }
+
+    @Test
+    void shouldDetectMissingTableInNamespace() {
+        List<String> excludedTables = Lists.newArrayList(Table.ROUTES.name, Table.TRIPS.name);
+        GtfsUtils.NamespaceInfo nsInfo = new GtfsUtils.NamespaceInfo("namespace", excludedTables);
+        assertEquals(
+            String.join(",", excludedTables),
+            String.join(",", nsInfo.missingTables)
+        );
+    }
+
+    @Test
+    void shouldDetectMissingTableColumns() {
+        List<String> removedColumns = Lists.newArrayList("route_id", "route_short_name");
+        List<GtfsUtils.ColumnInfo> columns = Arrays
+            .stream(Table.ROUTES.fields)
+            .filter(f -> !removedColumns.contains(f.name))
+            .map(f -> new GtfsUtils.ColumnInfo(f.name, f.getSqlTypeName()))
+            .collect(Collectors.toList());
+
+        GtfsUtils.TableInfo tableInfo = new GtfsUtils.TableInfo(Table.ROUTES, columns);
+        assertEquals(
+            String.join(",", removedColumns),
+            String.join(",", tableInfo.missingColumns)
+        );
+    }
 }
