ce776650df0f8e7ce2e81ef91c0db6fd82a48a54
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-04-22 08:41:14 -0400

    refactor(SqlSchemaUpdater): Close field statement on class close.

--- src/main/java/com/conveyal/datatools/manager/utils/sql/SqlSchemaUpdater.java
+++ src/main/java/com/conveyal/datatools/manager/utils/sql/SqlSchemaUpdater.java
@@ -243,5 +243,6 @@ public class SqlSchemaUpdater implements AutoCloseable {
     public void close() throws Exception {
         selectColumnStatement.close();
         selectNamespaceTablesStatement.close();
+        selectFeedStatement.close();
     }
 }
