a6a72a7890847b13b4d7b604b09b04427e62c974
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2022-05-13 16:59:24 -0400

    refactor(FeedVersionSummary): Expose only startDate and endDate in validationSummary.

--- src/main/java/com/conveyal/datatools/manager/models/FeedVersionSummary.java
+++ src/main/java/com/conveyal/datatools/manager/models/FeedVersionSummary.java
@@ -1,8 +1,13 @@
 package com.conveyal.datatools.manager.models;
 
+import com.conveyal.datatools.editor.utils.JacksonSerializers;
 import com.conveyal.gtfs.validator.ValidationResult;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
 
 /**
@@ -19,10 +24,40 @@ public class FeedVersionSummary extends Model implements Serializable {
     public String originNamespace;
     public Long fileSize;
     public Date updated;
+    /** Only a subset of the validation results are serialized to JSON via getValidationSummary. */
+    @JsonIgnore
     public ValidationResult validationResult;
+    private PartialValidationSummary validationSummary;
+
+    public PartialValidationSummary getValidationSummary() {
+        if (validationSummary == null) {
+            validationSummary = new PartialValidationSummary();
+        }
+        return validationSummary;
+    }
 
     /** Empty constructor for serialization */
     public FeedVersionSummary() {
         // Do nothing
+    }
+
+    /**
+     * Holds a subset of fields from {@link:FeedValidationResultSummary} for UI use only.
+     */
+    public class PartialValidationSummary {
+        /** Copied from FeedVersion */
+        @JsonSerialize(using = JacksonSerializers.LocalDateIsoSerializer.class)
+        @JsonDeserialize(using = JacksonSerializers.LocalDateIsoDeserializer.class)
+        public LocalDate startDate;
+
+        /** Copied from FeedVersion */
+        @JsonSerialize(using = JacksonSerializers.LocalDateIsoSerializer.class)
+        @JsonDeserialize(using = JacksonSerializers.LocalDateIsoDeserializer.class)
+        public LocalDate endDate;
+
+        PartialValidationSummary() {
+            this.startDate = validationResult.firstCalendarDate;
+            this.endDate = validationResult.lastCalendarDate;
+        }
     }
 }
