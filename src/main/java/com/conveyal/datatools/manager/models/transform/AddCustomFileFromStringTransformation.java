efa0acc67e33eca5a572ae96ee028ea3db28b2f2
philip.cline <philip.cline@ibigroup.com>
2023-06-08 12:46:58 -0400

    refactor(custom transformation): add separate class for custom file transformation

---
+++ src/main/java/com/conveyal/datatools/manager/models/transform/AddCustomFileFromStringTransformation.java
@@ -0,0 +1,13 @@
+package com.conveyal.datatools.manager.models.transform;
+import com.conveyal.datatools.common.status.MonitorableJob;
+
+public class AddCustomFileFromStringTransformation extends ReplaceFileFromStringTransformation {
+
+    @Override
+    public void validateTableName(MonitorableJob.Status status) {
+        if (table.contains(".txt")) {
+            status.fail("CSV Table name should not contain .txt");
+        }
+    }
+
+}
