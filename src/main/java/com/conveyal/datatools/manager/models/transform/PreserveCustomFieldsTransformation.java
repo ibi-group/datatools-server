f6b44db22b845754a94c4d938cee717100d9f036
philip.cline <philip.cline@ibigroup.com>
2023-06-09 19:51:29 -0400

    refactor(custom transformation): respond to PR comments

--- src/main/java/com/conveyal/datatools/manager/models/transform/PreserveCustomFieldsTransformation.java
+++ src/main/java/com/conveyal/datatools/manager/models/transform/PreserveCustomFieldsTransformation.java
@@ -41,18 +41,11 @@ public class PreserveCustomFieldsTransformation extends ZipTransformation {
         }
     }
 
-    public static <T> Collector<T, ?, T> toSingleton() {
-        return Collectors.collectingAndThen(
-                Collectors.toList(),
-                list -> {
-                    if (list.size() != 1) {
-                        throw new IllegalStateException();
-                    }
-                    return list.get(0);
-                }
-        );
-    }
-
+    /**
+     * This method creates a hash map of the GTFS table keys to the custom CSV values for efficient lookup of custom values.
+     * The hash map key is the key values of the GTFS table (e.g. stop_id for stops) concatenated by an underscore.
+     * The hash map value is the CsvMapReader (mapping of column to row value)
+     */
     private static HashMap<String, Map<String, String>> createCsvHashMap(CsvMapReader reader, String[] headers) throws IOException {
         HashMap<String, Map<String, String>> lookup = new HashMap<>();
         Map<String, String> nextLine;
@@ -69,61 +62,61 @@ public class PreserveCustomFieldsTransformation extends ZipTransformation {
         String tableName = table + ".txt";
         Path targetZipPath = Paths.get(zipTarget.gtfsFile.getAbsolutePath());
 
-        // TODO: is there a better way to do this than using a Singleton collector?
         specTable = Arrays.stream(Table.tablesInOrder)
-                .filter(t -> t.name.equals(table))
-                .collect(toSingleton());
+            .filter(t -> t.name.equals(table))
+            .findFirst()
+            .get();
+        List<String> specTableFields = specTable.specFields().stream().map(f -> f.name).collect(Collectors.toList());
         tablePrimaryKeys = specTable.getPrimaryKeyNames();
 
-        try( FileSystem targetZipFs = FileSystems.newFileSystem(targetZipPath, (ClassLoader) null) ){
-            List<String> specTableFields = specTable.specFields().stream().map(f -> f.name).collect(Collectors.toList());
+        try(FileSystem targetZipFs = FileSystems.newFileSystem( targetZipPath,(ClassLoader) null)){
             Path targetTxtFilePath = getTablePathInZip(tableName, targetZipFs);
 
-            // TODO: There must be a better way to do this.
             InputStream is = Files.newInputStream(targetTxtFilePath);
             final File tempFile = File.createTempFile(tableName + "-temp", ".txt");
             File output = File.createTempFile(tableName + "-output-temp", ".txt");
-            FileOutputStream out = new FileOutputStream(tempFile);
-            IOUtils.copy(is, out);
 
-            CsvMapReader newEditorFileReader = new CsvMapReader(new FileReader(tempFile), CsvPreference.STANDARD_PREFERENCE);
-            CsvMapReader newCustomFileReader = new CsvMapReader(new StringReader(csvData), CsvPreference.STANDARD_PREFERENCE);
+            try (
+                CsvMapReader customFileReader = new CsvMapReader(new StringReader(csvData), CsvPreference.STANDARD_PREFERENCE);
+                CsvMapReader editorFileReader = new CsvMapReader(new InputStreamReader(is), CsvPreference.STANDARD_PREFERENCE);
+                CsvMapWriter writer = new CsvMapWriter(new FileWriter(output), CsvPreference.STANDARD_PREFERENCE);
+            ){
 
-            String[] customHeaders = newCustomFileReader.getHeader(true);
-            final String[] editorHeaders = newEditorFileReader.getHeader(true);
+                String[] customHeaders = customFileReader.getHeader(true);
+                final String[] editorHeaders = editorFileReader.getHeader(true);
 
-            List<String> customFields = Arrays.stream(customHeaders).filter(h -> !specTableFields.contains(h)).collect(Collectors.toList());
-            if (customFields.size() == 0) return;
-            String[] fullHeaders = ArrayUtils.addAll(editorHeaders, customFields.toArray(new String[0]));
+                List<String> customFields = Arrays.stream(customHeaders).filter(h -> !specTableFields.contains(h)).collect(Collectors.toList());
+                if (customFields.size() == 0) return;
+                String[] fullHeaders = ArrayUtils.addAll(editorHeaders, customFields.toArray(new String[0]));
 
-            HashMap<String, Map<String, String>> lookup = createCsvHashMap(newCustomFileReader, customHeaders);
-            CsvMapWriter writer = new CsvMapWriter(new FileWriter(output), CsvPreference.STANDARD_PREFERENCE);
-            writer.writeHeader(fullHeaders);
+                HashMap<String, Map<String, String>> customFieldsLookup = createCsvHashMap(customFileReader, customHeaders);
+                writer.writeHeader(fullHeaders);
 
-            Map<String, String> row;
-            while ((row = newEditorFileReader.read(editorHeaders)) != null) {
-                List<String> editorCsvPrimaryKeyValues = tablePrimaryKeys.stream()
-                    .map(row::get)
-                    .collect(Collectors.toList());
+                Map<String, String> row;
+                while ((row = editorFileReader.read(editorHeaders)) != null) {
+                    List<String> editorCsvPrimaryKeyValues = tablePrimaryKeys.stream()
+                            .map(row::get)
+                            .collect(Collectors.toList());
 
-                String hashKey = StringUtils.join(editorCsvPrimaryKeyValues, "_");
-                Map<String, String> customCsvValues = lookup.get(hashKey);
-                Map<String, String> finalRow = row;
-                customFields.stream().forEach(customField -> {
-                    String value = customCsvValues == null ? null : customCsvValues.get(customField);
-                    finalRow.put(customField, value);
-                });
-                writer.write(finalRow, fullHeaders);
+                    String hashKey = StringUtils.join(editorCsvPrimaryKeyValues, "_");
+                    Map<String, String> customCsvValues = customFieldsLookup.get(hashKey);
+                    Map<String, String> finalRow = row;
+                    customFields.stream().forEach(customField -> {
+                        String value = customCsvValues == null ? null : customCsvValues.get(customField);
+                        finalRow.put(customField, value);
+                    });
+                    writer.write(finalRow, fullHeaders);
+                }
+                writer.close();
+
+                Files.copy(output.toPath(), targetTxtFilePath, StandardCopyOption.REPLACE_EXISTING);
+                tempFile.deleteOnExit();
+                output.deleteOnExit();
+                zipTarget.feedTransformResult.tableTransformResults.add(new TableTransformResult(tableName, TransformType.TABLE_MODIFIED));
             }
-            writer.close();
-
-            Files.copy(output.toPath(), targetTxtFilePath, StandardCopyOption.REPLACE_EXISTING);
-            tempFile.deleteOnExit();
-            output.deleteOnExit();
-            zipTarget.feedTransformResult.tableTransformResults.add(new TableTransformResult(tableName, TransformType.TABLE_MODIFIED));
         } catch (NoSuchFileException e) {
             status.fail("Source version does not contain table: " + tableName, e);
-        } catch(IOException e) {
+        } catch (IOException e) {
             status.fail("An exception occurred when writing output with custom fields", e);
         } catch (Exception e) {
             status.fail("Unknown error encountered while transforming zip file", e);
