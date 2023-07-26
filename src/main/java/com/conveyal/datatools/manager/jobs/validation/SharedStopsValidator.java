75c1904c2f8016606b6d144a2055d365b39e0a5a
miles-grant-ibi <miles.grant@ibigroup.com>
2023-07-27 01:18:56 +0200

    refactor: shared stops use all error types

--- src/main/java/com/conveyal/datatools/manager/jobs/validation/SharedStopsValidator.java
+++ src/main/java/com/conveyal/datatools/manager/jobs/validation/SharedStopsValidator.java
@@ -1,10 +1,15 @@
 package com.conveyal.datatools.manager.jobs.validation;
 
 import com.conveyal.datatools.manager.models.Project;
+import com.conveyal.gtfs.error.GTFSError;
+import com.conveyal.gtfs.error.NewGTFSError;
 import com.conveyal.gtfs.error.NewGTFSErrorType;
 import com.conveyal.gtfs.error.SQLErrorStorage;
 import com.conveyal.gtfs.loader.Feed;
+import com.conveyal.gtfs.loader.Table;
+import com.conveyal.gtfs.model.Entity;
 import com.conveyal.gtfs.model.Route;
+import com.conveyal.gtfs.model.Stop;
 import com.conveyal.gtfs.validator.FeedValidator;
 
 
@@ -33,8 +38,16 @@ public class SharedStopsValidator extends FeedValidator {
     @Override
     public void validate() {
 
-        for (Route route : feed.routes) {
-            registerError(route, NewGTFSErrorType.MULTIPLE_SHARED_STOPS_GROUPS);
+        String config = project.sharedStopsConfig;
+
+
+        for (Stop stop : feed.stops) {
+            registerError(stop, NewGTFSErrorType.MULTIPLE_SHARED_STOPS_GROUPS);
+        }
+        if (config == null) {
+            registerError(NewGTFSError.forFeed(NewGTFSErrorType.SHARED_STOP_GROUP_ENTITY_DOES_NOT_EXIST, "uh oh"));
+            registerError(NewGTFSError.forFeed(NewGTFSErrorType.SHARED_STOP_GROUP_MUTLIPLE_PRIMARY_STOPS, "good god"));
+
         }
 
     }
