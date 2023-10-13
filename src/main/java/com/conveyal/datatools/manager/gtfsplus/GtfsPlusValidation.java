68cec40c0258cdfbe46521a5aa79f5af8425f5df
philip.cline <philip.cline@ibigroup.com>
2023-10-13 11:35:47 -0400

    refactor(Directions Validation): update validation language

--- src/main/java/com/conveyal/datatools/manager/gtfsplus/GtfsPlusValidation.java
+++ src/main/java/com/conveyal/datatools/manager/gtfsplus/GtfsPlusValidation.java
@@ -188,7 +188,7 @@ public class GtfsPlusValidation implements Serializable {
 
         if (tableIsDirections && !gtfsRoutes.isEmpty()) {
             // After we're done validating all the table values, check if every route was checked off in directions.txt
-            issues.add(new ValidationIssue(tableId, null, -1, "Directions file doesn't define directions for all routes listed in routes file"));
+            issues.add(new ValidationIssue(tableId, null, -1, "Directions file doesn't define directions for all routes listed in routes.txt"));
         }
         // Add issues for wrong number of columns and for empty rows after processing all rows.
         // Note: We considered adding an issue for each row, but opted for the single error approach because there's no
