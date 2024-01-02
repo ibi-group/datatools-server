408924459eccdbc0bebe1243b03748896693253a
miles-grant-ibi <miles.grant@ibigroup.com>
2024-01-02 22:21:14 +0100

    correct nonce script (quotes)

--- docker-stuff/add-nonce.sh
+++ docker-stuff/add-nonce.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apk add jq
-export nonce=$(jq .nonce /var/opentripplanner/otp-runner-server-only-manifest.json)
+export nonce=$(jq -r .nonce /var/opentripplanner/otp-runner-server-only-manifest.json)
 touch /etc/caddy/static/status-new.json
-jq '.nonce = \"$nonce\"' /etc/caddy/static/status.json > /etc/caddy/static/status-new.json
+cat /etc/caddy/static/status.json | jq --arg n $nonce '.nonce = $n' > /etc/caddy/static/status-new.json
 rm /etc/caddy/static/status.json
 mv /etc/caddy/static/status-new.json /etc/caddy/static/status.json
