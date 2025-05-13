958abd085bf2aa33494c39b7ecbc97d844b99615
Robin Beer <robin.beer@arcadis.com>
2025-05-13 13:59:29 +0100

    improvement(FeedVersion.java): Removed unused import

--- src/main/java/com/conveyal/datatools/manager/models/FeedVersion.java
+++ src/main/java/com/conveyal/datatools/manager/models/FeedVersion.java
@@ -48,7 +48,6 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
