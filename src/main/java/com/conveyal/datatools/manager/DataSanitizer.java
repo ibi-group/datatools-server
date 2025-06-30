4b17d79607341d6ace0934e7854108d616bf2453
Robin Beer <robin.beer@arcadis.com>
2025-06-30 15:23:06 +0100

    feat(Added the ability to remove orphaned DB schemas): Delete all DB schemas that are not associated

--- src/main/java/com/conveyal/datatools/manager/DataSanitizer.java
+++ src/main/java/com/conveyal/datatools/manager/DataSanitizer.java
@@ -4,10 +4,22 @@ import com.conveyal.datatools.common.utils.aws.CheckedAWSException;
 import com.conveyal.datatools.manager.models.FeedSource;
 import com.conveyal.datatools.manager.models.FeedVersion;
 import com.conveyal.datatools.manager.persistence.Persistence;
+import com.conveyal.gtfs.GTFS;
 import com.conveyal.gtfs.util.InvalidNamespaceException;
+import com.conveyal.gtfs.util.Util;
+import com.google.common.collect.Lists;
+import com.mongodb.client.model.Projections;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -15,7 +27,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.conveyal.datatools.manager.DataManager.GTFS_DATA_SOURCE;
 import static com.conveyal.datatools.manager.DataManager.initializeApplication;
+import static com.mongodb.client.model.Aggregates.project;
 import static com.mongodb.client.model.Filters.nin;
 
 /**
@@ -24,6 +38,7 @@ import static com.mongodb.client.model.Filters.nin;
  * configurations/test/env.yml.tmp configurations/test/server.yml.tmp --orphaned --delete
  */
 public class DataSanitizer {
+    private static final Logger LOG = LoggerFactory.getLogger(DataSanitizer.class);
     public static final List<String> ORPHANED_FLAGS = List.of("--orphaned", "-O");
     public static final List<String> DELETE_FLAGS = List.of("--delete", "-D");
 
@@ -41,6 +56,7 @@ public class DataSanitizer {
         groupedParams.forEach((flag, commandValues) -> {
             if (ORPHANED_FLAGS.contains(flag)) {
                 sanitizeFeedVersions(DELETE_FLAGS.contains(flag));
+                sanitizeDBSchemas(DELETE_FLAGS.contains(flag));
             }
         });
     }
@@ -64,40 +80,86 @@ public class DataSanitizer {
     }
 
     /**
-     * Group orphaned feed versions and delete.
+     * Group orphaned feed versions and optionally delete.
      */
     public static int sanitizeFeedVersions(boolean delete) {
         List<FeedVersion> feedVersions = getOrphanedFeedVersions();
         int orphaned = feedVersions.size();
         if (orphaned == 0) {
-            System.out.println("No orphaned versions found!");
+            System.out.println("No orphaned feed versions found!");
         } else {
             System.out.printf("Orphaned feed versions (%s)%n", orphaned);
             for (FeedVersion feedVersion : feedVersions) {
                 System.out.printf("%-10s | %-10s | %-10s | %-10s%n", "ID", "Version", "Created", "Updated");
                 System.out.printf(
                     "%-10s | %-10s | %-10s | %-10s%n",
-                    feedVersion.id,
+                    feedVersion.name,
                     feedVersion.version,
                     feedVersion.dateCreated,
                     feedVersion.lastUpdated
                 );
             }
         }
+
         if (delete && !feedVersions.isEmpty()) {
-            int deletedFeedVersions = 0;
-            for (FeedVersion feedVersion : feedVersions) {
-                try {
-                    System.out.println("Deleting orphaned feed version: " + feedVersion.id);
-                    feedVersion.deleteOrphan();
-                } catch (SQLException | CheckedAWSException | InvalidNamespaceException e) {
-                    System.err.printf("Failed to delete feed version: %s. %s%n", feedVersion.id, e.getMessage());
-                }
-                deletedFeedVersions++;
-            }
-            System.out.println("Total orphaned feed versions deleted: " + deletedFeedVersions);
+            System.out.println("Total orphaned feed versions deleted: " + deleteOrphanedFeedVersions(feedVersions));
         }
         return orphaned;
+    }
+
+    /**
+     * Group orphaned schemas and optionally delete.
+     */
+    public static int sanitizeDBSchemas(boolean delete) {
+        List<String> orphanedSchemas = getOrphanedDBSchemas(getFeedVersionSchemas());
+        if (orphanedSchemas.isEmpty()) {
+            System.out.println("No orphaned DB schemas found!");
+
+        } else {
+            System.out.println("Orphaned DB schemas: " + orphanedSchemas.size());
+            for (String schema : orphanedSchemas) {
+                System.out.println(schema);
+            }
+        }
+
+        if (delete && !orphanedSchemas.isEmpty()) {
+            System.out.println("Total orphaned DB schemas deleted: " + deleteOrphanedDBSchemas(orphanedSchemas));
+        }
+        return orphanedSchemas.size();
+    }
+
+    /**
+     * Delete orphaned feed versions.
+     */
+    private static int deleteOrphanedFeedVersions(List<FeedVersion> feedVersions) {
+        int deletedFeedVersions = 0;
+        for (FeedVersion feedVersion : feedVersions) {
+            try {
+                System.out.println("Deleting orphaned feed version: " + feedVersion.id);
+                feedVersion.deleteOrphan();
+            } catch (SQLException | CheckedAWSException | InvalidNamespaceException e) {
+                System.err.printf("Failed to delete feed version: %s. %s%n", feedVersion.id, e.getMessage());
+            }
+            deletedFeedVersions++;
+        }
+        return deletedFeedVersions;
+    }
+
+    /**
+     * Delete orphaned DB schemas.
+     */
+    public static int deleteOrphanedDBSchemas(List<String> orphanedSchemas) {
+        int deletedSchemas = 0;
+        for (String orphanedSchema : orphanedSchemas) {
+            try {
+                GTFS.delete(orphanedSchema, DataManager.GTFS_DATA_SOURCE);
+                LOG.info("Dropped orphaned DB schema from Postgres.");
+            } catch (SQLException | InvalidNamespaceException e) {
+                System.err.printf("Failed to delete DB schema: %s. %s%n", orphanedSchema, e.getMessage());
+            }
+            deletedSchemas++;
+        }
+        return deletedSchemas;
     }
 
     /**
@@ -110,5 +172,66 @@ public class DataSanitizer {
             .map(feedSource -> feedSource.id)
             .collect(Collectors.toSet());
         return Persistence.feedVersions.getFiltered(nin("feedSourceId", feedSourceIds));
+    }
+
+    /**
+     * Get all qualifying schemas that are not associated with a feed version.
+     */
+    public static List<String> getOrphanedDBSchemas(List<String> associatedSchemas) {
+        String whereClause = associatedSchemas.isEmpty() ? "" : String.format(" WHERE nspname NOT IN (%s)", associatedSchemas
+            .stream()
+            .map(schema -> "'" + schema + "'")
+            .collect(Collectors.joining(", "))
+        );
+        List<String> orphanedSchemas = new ArrayList<>();
+        try (Connection connection = GTFS_DATA_SOURCE.getConnection()) {
+            String sql = String.format("SELECT nspname FROM pg_namespace %s", whereClause);
+            LOG.info(sql);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String schemaName = resultSet.getString(1);
+                if (isValidSchema(schemaName)) {
+                    orphanedSchemas.add(schemaName);
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("Unable to get orphaned DB schemas", e);
+        }
+        return orphanedSchemas;
+    }
+
+    /**
+     * Make sure the schema qualifies as an OTP related schema.
+     */
+    private static boolean isValidSchema(String schemaName) {
+        List<String> criticalSchemas = List.of("catalog", "information_schema", "public", "temp", "toast");
+        try {
+            Util.ensureValidNamespace(schemaName);
+            if (criticalSchemas.stream().noneMatch(schemaName::contains)) {
+                // Belts and braces in case the previous check changes.
+                return true;
+            }
+        } catch (InvalidNamespaceException e) {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Extract the namespace (schema) from all FeedVersion documents.
+     */
+    public static List<String> getFeedVersionSchemas() {
+        List<String> associatedSchemas = new ArrayList<>();
+
+        List<Bson> stages = Lists.newArrayList(
+            project(
+                Projections.fields(Projections.include("namespace"))
+            )
+        );
+        for (Document feedVersionDocument : Persistence.getDocuments("FeedVersion", stages)) {
+            associatedSchemas.add(feedVersionDocument.getString("namespace"));
+        }
+        return associatedSchemas;
     }
 }
--- src/test/java/com/conveyal/datatools/DataSanitizerTest.java
+++ src/test/java/com/conveyal/datatools/DataSanitizerTest.java
@@ -1,12 +1,15 @@
 package com.conveyal.datatools;
 
 import com.conveyal.datatools.common.utils.aws.CheckedAWSException;
+import com.conveyal.datatools.manager.DataManager;
 import com.conveyal.datatools.manager.DataSanitizer;
 import com.conveyal.datatools.manager.auth.Auth0Connection;
+import com.conveyal.datatools.manager.jobs.ProcessSingleFeedJob;
 import com.conveyal.datatools.manager.models.FeedSource;
 import com.conveyal.datatools.manager.models.FeedVersion;
 import com.conveyal.datatools.manager.models.Project;
 import com.conveyal.datatools.manager.persistence.Persistence;
+import com.conveyal.gtfs.GTFS;
 import com.conveyal.gtfs.util.InvalidNamespaceException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,6 +17,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.conveyal.datatools.TestUtils.appendDate;
 import static com.conveyal.datatools.TestUtils.createFeedVersion;
@@ -22,18 +27,21 @@ import static com.conveyal.datatools.manager.models.FeedRetrievalMethod.MANUALLY
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DataSanitizerTest {
 
     private static Project project;
     private static FeedVersion feedVersionOrphan;
     private static FeedVersion feedVersionWithParent;
+    private static String orphanedDBSchema;
 
     @BeforeAll
     static void setUp() throws IOException {
         // start server if it isn't already running
         DatatoolsTest.setUp();
         Auth0Connection.setAuthDisabled(true);
+        ProcessSingleFeedJob.VALIDATE_MOBILITY_DATA = false;
         project = new Project();
         project.name = appendDate("Test");
         Persistence.projects.create(project);
@@ -49,17 +57,30 @@ class DataSanitizerTest {
             feedSourceParent,
             zipFolderFiles("fake-agency-with-only-calendar")
         );
+        FeedVersion feedVersionWithOrphanDBSchema = createFeedVersion(
+            feedSourceParent,
+            zipFolderFiles("fake-agency-with-only-calendar")
+        );
+        orphanedDBSchema = feedVersionWithOrphanDBSchema.namespace;
         // Delete feed source to orphan feed version.
         Persistence.feedSources.removeById(feedSource.id);
+        // Delete feed version to orphan DB schema.
+        Persistence.feedVersions.removeById(feedVersionWithOrphanDBSchema.id);
     }
 
     @AfterAll
     static void tearDown() throws SQLException, InvalidNamespaceException, CheckedAWSException {
         Auth0Connection.setAuthDisabled(false);
+        ProcessSingleFeedJob.VALIDATE_MOBILITY_DATA = true;
         project.delete();
         FeedVersion feedVersion = Persistence.feedVersions.getById(feedVersionOrphan.id);
         if (feedVersion != null) {
             feedVersionOrphan.deleteOrphan();
+        }
+        try {
+            GTFS.delete(orphanedDBSchema, DataManager.GTFS_DATA_SOURCE);
+        } catch (Exception e) {
+          // Do nothing. Schema removed in unit test.
         }
     }
 
@@ -75,5 +96,17 @@ class DataSanitizerTest {
         assertEquals(1, DataSanitizer.sanitizeFeedVersions(true));
         assertNull(Persistence.feedVersions.getById(feedVersionOrphan.id));
         assertNotNull(Persistence.feedVersions.getById(feedVersionWithParent.id));
+    }
+
+    @Test
+    void canIdentifyOrphanedDBSchemas() {
+        // Other schemas will exist. For this test, just make sure the list contains the test orphaned schema.
+        List<String> orphanedSchemas = DataSanitizer.getOrphanedDBSchemas(new ArrayList<>());
+        assertTrue(orphanedSchemas.contains(orphanedDBSchema));
+    }
+
+    @Test
+    void canRemoveOrphanedDBSchema() {
+        assertEquals(1, DataSanitizer.deleteOrphanedDBSchemas(List.of(orphanedDBSchema)));
     }
 }
