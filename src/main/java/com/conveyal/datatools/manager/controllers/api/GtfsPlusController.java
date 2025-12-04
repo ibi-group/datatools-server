424d78a7eeb4fd764a4040b8399331e9dcd4edac
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-12-04 08:36:08 -0500

    fix(GtfsPlusController): Fix bad file init and add try-resources in gtfsplus publish.

--- src/main/java/com/conveyal/datatools/manager/controllers/api/GtfsPlusController.java
+++ src/main/java/com/conveyal/datatools/manager/controllers/api/GtfsPlusController.java
@@ -178,46 +178,50 @@ public class GtfsPlusController {
 
         File newFeed = null;
 
-        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(newFeed))) {
+        try {
             // First, create a new zip file to only contain the GTFS+ tables
             newFeed = File.createTempFile(feedVersionId + "_new", ".zip");
+            try (
+                ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(newFeed));
+                ZipFile gtfsFile = new ZipFile(feedVersion.retrieveGtfsFile())
+            ) {
+                // Next, iterate through the existing GTFS file, copying all non-GTFS+ tables.
+                final Enumeration<? extends ZipEntry> entries = gtfsFile.entries();
+                byte[] buffer = new byte[512];
+                while (entries.hasMoreElements()) {
+                    final ZipEntry entry = entries.nextElement();
+                    // skip GTFS+ and non-standard tables
+                    if (gtfsPlusTables.contains(entry.getName()) || entry.getName().startsWith("_")) continue;
 
-            // Next, iterate through the existing GTFS file, copying all non-GTFS+ tables.
-            ZipFile gtfsFile = new ZipFile(feedVersion.retrieveGtfsFile());
-            final Enumeration<? extends ZipEntry> entries = gtfsFile.entries();
-            byte[] buffer = new byte[512];
-            while (entries.hasMoreElements()) {
-                final ZipEntry entry = entries.nextElement();
-                // skip GTFS+ and non-standard tables
-                if (gtfsPlusTables.contains(entry.getName()) || entry.getName().startsWith("_")) continue;
+                    // create a new empty ZipEntry and copy the contents
+                    ZipEntry newEntry = new ZipEntry(entry.getName());
+                    zos.putNextEntry(newEntry);
+                    try (InputStream in = gtfsFile.getInputStream(entry)) {
+                        while (0 < in.available()) {
+                            int read = in.read(buffer);
+                            zos.write(buffer, 0, read);
+                        }
+                    }
+                    zos.closeEntry();
+                }
 
-                // create a new empty ZipEntry and copy the contents
-                ZipEntry newEntry = new ZipEntry(entry.getName());
-                zos.putNextEntry(newEntry);
-                try (InputStream in = gtfsFile.getInputStream(entry)) {
-                    while (0 < in.available()) {
-                        int read = in.read(buffer);
-                        zos.write(buffer, 0, read);
+                // iterate through the GTFS+ file, copying all entries
+                try (ZipFile plusZipFile = new ZipFile(plusFile)) {
+                    final Enumeration<? extends ZipEntry> plusEntries = plusZipFile.entries();
+                    while (plusEntries.hasMoreElements()) {
+                        final ZipEntry entry = plusEntries.nextElement();
+
+                        ZipEntry newEntry = new ZipEntry(entry.getName());
+                        zos.putNextEntry(newEntry);
+                        try (InputStream in = plusZipFile.getInputStream(entry)) {
+                            while (0 < in.available()) {
+                                int read = in.read(buffer);
+                                zos.write(buffer, 0, read);
+                            }
+                        }
+                        zos.closeEntry();
                     }
                 }
-                zos.closeEntry();
-            }
-
-            // iterate through the GTFS+ file, copying all entries
-            ZipFile plusZipFile = new ZipFile(plusFile);
-            final Enumeration<? extends ZipEntry> plusEntries = plusZipFile.entries();
-            while (plusEntries.hasMoreElements()) {
-                final ZipEntry entry = plusEntries.nextElement();
-
-                ZipEntry newEntry = new ZipEntry(entry.getName());
-                zos.putNextEntry(newEntry);
-                try (InputStream in = plusZipFile.getInputStream(entry)) {
-                    while (0 < in.available()) {
-                        int read = in.read(buffer);
-                        zos.write(buffer, 0, read);
-                    }
-                }
-                zos.closeEntry();
             }
         } catch (IOException e) {
             logMessageAndHalt(req, 500, "Error creating combined GTFS/GTFS+ file", e);
