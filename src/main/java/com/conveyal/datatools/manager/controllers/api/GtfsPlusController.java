36ab110b0e4270baca07cdf98a4d17714e4bce28
binh-dam-ibigroup <binh.dam@arcadis.com>
2025-12-04 08:27:15 -0500

    fix(GtfsPlusController): Fix bad file init.

--- src/main/java/com/conveyal/datatools/manager/controllers/api/GtfsPlusController.java
+++ src/main/java/com/conveyal/datatools/manager/controllers/api/GtfsPlusController.java
@@ -100,28 +100,31 @@ public class GtfsPlusController {
             gtfsPlusTables.add(tableNode.get("name").asText());
         }
 
-        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(gtfsPlusFile))) {
+        try {
             // create a new zip file to only contain the GTFS+ tables
             gtfsPlusFile = File.createTempFile(version.id + "_gtfsplus", ".zip");
+            try (
+                ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(gtfsPlusFile));
+                ZipFile gtfsFile = new ZipFile(version.retrieveGtfsFile())
+            ) {
+                // iterate through the existing GTFS file, copying any GTFS+ tables
+                final Enumeration<? extends ZipEntry> entries = gtfsFile.entries();
+                byte[] buffer = new byte[512];
+                while (entries.hasMoreElements()) {
+                    final ZipEntry entry = entries.nextElement();
+                    if (!gtfsPlusTables.contains(entry.getName())) continue;
 
-            // iterate through the existing GTFS file, copying any GTFS+ tables
-            ZipFile gtfsFile = new ZipFile(version.retrieveGtfsFile());
-            final Enumeration<? extends ZipEntry> entries = gtfsFile.entries();
-            byte[] buffer = new byte[512];
-            while (entries.hasMoreElements()) {
-                final ZipEntry entry = entries.nextElement();
-                if (!gtfsPlusTables.contains(entry.getName())) continue;
-
-                // create a new empty ZipEntry and copy the contents
-                ZipEntry newEntry = new ZipEntry(entry.getName());
-                zos.putNextEntry(newEntry);
-                try (InputStream in = gtfsFile.getInputStream(entry)) {
-                    while (0 < in.available()) {
-                        int read = in.read(buffer);
-                        zos.write(buffer, 0, read);
+                    // create a new empty ZipEntry and copy the contents
+                    ZipEntry newEntry = new ZipEntry(entry.getName());
+                    zos.putNextEntry(newEntry);
+                    try (InputStream in = gtfsFile.getInputStream(entry)) {
+                        while (0 < in.available()) {
+                            int read = in.read(buffer);
+                            zos.write(buffer, 0, read);
+                        }
                     }
+                    zos.closeEntry();
                 }
-                zos.closeEntry();
             }
         } catch (IOException e) {
             logMessageAndHalt(req, 500, "An error occurred while trying to create a gtfs file", e);
