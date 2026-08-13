95d8a6eaafb278c23a91ea62c8396e8eb9aa97e7
binh-dam-ibigroup <binh.dam@arcadis.com>
2026-08-13 11:22:18 -0400

    refactor: Convert EC2-related files to AWS SDK.

--- src/main/java/com/conveyal/datatools/manager/models/EC2InstanceSummary.java
+++ src/main/java/com/conveyal/datatools/manager/models/EC2InstanceSummary.java
@@ -1,8 +1,8 @@
 package com.conveyal.datatools.manager.models;
 
-import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.InstanceState;
-import com.amazonaws.services.ec2.model.Tag;
+import software.amazon.awssdk.services.ec2.model.Instance;
+import software.amazon.awssdk.services.ec2.model.InstanceState;
+import software.amazon.awssdk.services.ec2.model.Tag;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -32,23 +32,23 @@ public class EC2InstanceSummary implements Serializable {
     public EC2InstanceSummary () { }
 
     public EC2InstanceSummary (Instance ec2Instance) {
-        publicIpAddress = ec2Instance.getPublicIpAddress();
-        privateIpAddress = ec2Instance.getPrivateIpAddress();
-        publicDnsName = ec2Instance.getPublicDnsName();
-        instanceType = ec2Instance.getInstanceType();
-        instanceId = ec2Instance.getInstanceId();
-        imageId = ec2Instance.getImageId();
-        List<Tag> tags = ec2Instance.getTags();
+        publicIpAddress = ec2Instance.publicIpAddress();
+        privateIpAddress = ec2Instance.privateIpAddress();
+        publicDnsName = ec2Instance.publicDnsName();
+        instanceType = ec2Instance.instanceType().name();
+        instanceId = ec2Instance.instanceId();
+        imageId = ec2Instance.imageId();
+        List<Tag> tags = ec2Instance.tags();
         // Set project and deployment ID if they exist.
         for (Tag tag : tags) {
-            if (tag.getKey().equals("projectId")) projectId = tag.getValue();
-            if (tag.getKey().equals("deploymentId")) deploymentId = tag.getValue();
-            if (tag.getKey().equals("jobId")) jobId = tag.getValue();
-            if (tag.getKey().equals("Name")) name = tag.getValue();
+            if (tag.key().equals("projectId")) projectId = tag.value();
+            if (tag.key().equals("deploymentId")) deploymentId = tag.value();
+            if (tag.key().equals("jobId")) jobId = tag.value();
+            if (tag.key().equals("Name")) name = tag.value();
         }
-        state = ec2Instance.getState();
-        availabilityZone = ec2Instance.getPlacement().getAvailabilityZone();
-        launchTime = ec2Instance.getLaunchTime();
-        stateTransitionReason = ec2Instance.getStateTransitionReason();
+        state = ec2Instance.state();
+        availabilityZone = ec2Instance.placement().availabilityZone();
+        launchTime = Date.from(ec2Instance.launchTime());
+        stateTransitionReason = ec2Instance.stateTransitionReason();
     }
 }
